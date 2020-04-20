eff58ef1729ccc1ed8e1a1f6075eae830bd3a081
Vitor Kato <vitor48kato@gmail.com>
2020-04-20 12:40:32 -0300

    SellerService and basic FindAll

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
 {
     public class SellersController : Controller
     {
+
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService s)
+        {
+            _sellerService = s;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _sellerService.FindAll();
+
+            return View(list);                      //encaminha os dados para a view
         }
     }
 }
---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SellerService(SalesWebMVCContext c)
+        {
+            _context = c;
+        }
+
+        public List<Seller> FindAll()           //Retorna lista com todos os vendedores do banco
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
